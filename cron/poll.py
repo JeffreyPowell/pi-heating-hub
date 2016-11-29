a10b4d98e801fe4f23d8077a74fbbe19e8d10480
Jeff <Jffrypwll@googlemail.com>
2016-11-29 16:07:55 +0000

    Update poll.py

--- cron/poll.py
+++ cron/poll.py
@@ -1,1 +1,9 @@
 
+https://dev.mysql.com/doc/connector-python/en/connector-python-example-connecting.html
+
+#SQL Connection Test
+import MySQLdb
+db = MySQLdb.connect(host="localhost", user="pi", passwd="password", db="pi-heating-hub")
+
+cur = db.cursor()
+
