6a011b2b2d7ba0fbd8ca97f496c18c9f3218ffa9
Jeff <Jffrypwll@googlemail.com>
2016-11-30 11:34:35 +0000

    Update poll.py

--- cron/poll.py
+++ cron/poll.py
@@ -1,9 +1,48 @@
+#!/usr/bin/env python
 
-https://dev.mysql.com/doc/connector-python/en/connector-python-example-connecting.html
-
-#SQL Connection Test
+#import sys
+#import os
+#import logging
+#import pif
 import MySQLdb
+
+# https://dev.mysql.com/doc/connector-python/en/connector-python-example-connecting.html
+
+
+# GLOBALS
+MYSQL_USERNAME="USERNAME"
+MYSQL_PASSWORD="PASSWORD"
+
+
+ 
+#
+# check locally if IP has changed
+#
+def check_ip_file(public_ip):
+  if os.path.exists(IPFILE):
+    # Open a file
+    fo = open(IPFILE, "r")
+    old_ip = fo.read(50)
+    fo.close()
+    #print "Read String is : ", str
+    # Close opend file
+    if old_ip == public_ip:
+      print "ip is the same.. not doing anything"
+      return 1
+  # return if no file exists, or the IP is new
+  return
+ 
+
+ 
+### BEGIN MAIN PROCEDURE
+
+poll_all_sensors()
+public_ip = pif.get_public_ip()
+ 
+if check_ip_file(public_ip) != 1:
+  update_dns(public_ip)
+  
+  #SQL Connection Test
 db = MySQLdb.connect(host="localhost", user="pi", passwd="password", db="pi-heating-hub")
-
 cur = db.cursor()
-
+  
