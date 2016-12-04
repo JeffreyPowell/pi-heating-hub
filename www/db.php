3e377e4860376edec28736e345698cf070d1422d
Jeff <Jffrypwll@googlemail.com>
2016-12-04 18:00:04 +0000

    Update db.php

--- www/db.php
+++ www/db.php
@@ -2,13 +2,26 @@
 $servername = "localhost";
 $username = "pi";
 $password = "password";
+$dbname = "pi_heating_db";
 
 // Create connection
-$conn = new mysqli($servername, $username, $password);
-
+$conn = mysqli_connect($servername, $username, $password, $dbname);
 // Check connection
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-} 
-echo "Connected successfully";
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+}
+
+$sql = "SELECT * FROM schedules";
+$result = mysqli_query($conn, $sql);
+
+if (mysqli_num_rows($result) > 0) {
+    // output data of each row
+    while($row = mysqli_fetch_assoc($result)) {
+        echo var_dump($row)."<br>";
+    }
+} else {
+    echo "0 results";
+}
+
+mysqli_close($conn);
 ?>
