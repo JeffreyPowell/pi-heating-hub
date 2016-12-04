1c9ecdfa1b6d5d36b6cc94fce0e0db08121a8357
Jeff <Jffrypwll@googlemail.com>
2016-12-04 23:36:42 +0000

    Update sched-list.php

--- www/sched-list.php
+++ www/sched-list.php
@@ -38,7 +38,7 @@ if (mysqli_num_rows($result) > 0) {
     for ($i=1; $i<8; $i++) {
       echo $i;
       }
-    echo "<td><span class='ccolvalue'>".$row["value"]."</span></td>";
+    echo "<td><span class='ccolvalue'>".decbin($row["value"])."</span></td>";
     
     echo "<td><form method='post' action='sched-edit.php?id=".$row["id"]."'>";
     echo "<input type='submit' name='edit' value='Edit'></form></td>";
