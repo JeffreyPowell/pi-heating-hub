62da6b8fbee4eaccff79693bb99332c30cf9edfd
Jeff <Jffrypwll@googlemail.com>
2016-12-05 21:12:06 +0000

    Update sched-edit.php

--- www/sched-edit.php
+++ www/sched-edit.php
@@ -8,6 +8,57 @@
 <body>  
 
 <?php
+$servername = "localhost";
+$username = "pi";
+$password = "password";
+$dbname = "pi_heating_db";
+// Create connection
+$conn = mysqli_connect($servername, $username, $password, $dbname);
+// Check connection
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+}
+$sql = "SELECT * FROM schedules";
+$result = mysqli_query($conn, $sql);
+if (mysqli_num_rows($result) > 0) {
+    // output data of each row
+  
+  echo "<table><tr><th><span class='tcolname'>Schedule Name</span></th><th>Start Time</th><th>End Time</th><th>Repeat<br>MTWTFSS</th><th>Status</th><th></th><th></th></tr>";
+  
+  while($row = mysqli_fetch_assoc($result)) {
+    echo "<tr>";
+    echo "<td><span class='ccolname'>".$row["name"]."</span></td>";
+    echo "<td><span class='ccolstart'>".$row["start"]."</span></td>";
+    echo "<td><span class='ccolend'>".$row["end"]."</span></td>";
+      
+    echo "<td><span class='ccoldowun'>";
+    echo str_pad(decbin($row["dow"]), 7, "0", STR_PAD_LEFT);
+      
+    for ($i=1; $i<8; $i++) {
+ //     echo $i;
+      }
+    echo "</span></td>";
+      
+    echo "<td><span class='ccolvalue'>".$row["value"]."</span></td>";
+    
+    echo "<td><form method='post' action='sched-edit.php?id=".$row["id"]."'>";
+    echo "<input type='submit' name='edit' value='Edit'></form></td>";
+    echo "<td><form method='post' action='sched-delete.php?id=".$row["id"]."'>";
+    echo "<input type='submit' name='delete' value='Delete'></form></td>";
+    echo "</tr>";
+  }    
+  
+  echo "</table>";
+  
+} else {
+    echo "0 results";
+}
+  
+mysqli_close($conn);
+?>  
+
+  
+<?php
 // define variables and set to empty values
 $nameErr = $emailErr = $genderErr = $websiteErr = "";
 $name = $email = $gender = $comment = $website = "";
@@ -81,68 +132,6 @@ function test_input($data) {
   <br><br>
   <input type="submit" name="submit" value="Submit">  
 </form>
-
-<?php
-echo "<h2>Your Input:</h2>";
-echo $name;
-echo "<br>";
-echo $email;
-echo "<br>";
-echo $website;
-echo "<br>";
-echo $comment;
-echo "<br>";
-echo $gender;
-?>
-<?php
-$servername = "localhost";
-$username = "pi";
-$password = "password";
-$dbname = "pi_heating_db";
-// Create connection
-$conn = mysqli_connect($servername, $username, $password, $dbname);
-// Check connection
-if (!$conn) {
-    die("Connection failed: " . mysqli_connect_error());
-}
-$sql = "SELECT * FROM schedules";
-$result = mysqli_query($conn, $sql);
-if (mysqli_num_rows($result) > 0) {
-    // output data of each row
-  
-  echo "<table><tr><th><span class='tcolname'>Schedule Name</span></th><th>Start Time</th><th>End Time</th><th>Repeat<br>MTWTFSS</th><th>Status</th><th></th><th></th></tr>";
-  
-  while($row = mysqli_fetch_assoc($result)) {
-    echo "<tr>";
-    echo "<td><span class='ccolname'>".$row["name"]."</span></td>";
-    echo "<td><span class='ccolstart'>".$row["start"]."</span></td>";
-    echo "<td><span class='ccolend'>".$row["end"]."</span></td>";
-      
-    echo "<td><span class='ccoldowun'>";
-    echo str_pad(decbin($row["dow"]), 7, "0", STR_PAD_LEFT);
-      
-    for ($i=1; $i<8; $i++) {
- //     echo $i;
-      }
-    echo "</span></td>";
-      
-    echo "<td><span class='ccolvalue'>".$row["value"]."</span></td>";
-    
-    echo "<td><form method='post' action='sched-edit.php?id=".$row["id"]."'>";
-    echo "<input type='submit' name='edit' value='Edit'></form></td>";
-    echo "<td><form method='post' action='sched-delete.php?id=".$row["id"]."'>";
-    echo "<input type='submit' name='delete' value='Delete'></form></td>";
-    echo "</tr>";
-  }    
-  
-  echo "</table>";
-  
-} else {
-    echo "0 results";
-}
-  
-mysqli_close($conn);
-?>  
 
 
 </body>
