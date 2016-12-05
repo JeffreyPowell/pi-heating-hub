bfd835471026824bc3629141081d46a49982aad7
Jeff <Jffrypwll@googlemail.com>
2016-12-05 22:01:09 +0000

    Update sched-edit.php

--- www/sched-edit.php
+++ www/sched-edit.php
@@ -21,40 +21,11 @@ if (!$conn) {
     die("Connection failed: " . mysqli_connect_error());
 }
 $sql = "SELECT * FROM schedules WHERE id=".$SCHED_ID;
-$result = mysqli_query($conn, $sql);
-if (mysqli_num_rows($result) > 0) {
-    // output data of each row
+$result_sched = mysqli_query($conn, $sql);
+
+echo var_dump( $result_sched ); 
   
-  echo "<table><tr><th><span class='tcolname'>Schedule Name</span></th><th>Start Time</th><th>End Time</th><th>Repeat<br>MTWTFSS</th><th>Status</th><th></th><th></th></tr>";
-  
-  while($row = mysqli_fetch_assoc($result)) {
-    echo "<tr>";
-    echo "<td><span class='ccolname'>".$row["name"]."</span></td>";
-    echo "<td><span class='ccolstart'>".$row["start"]."</span></td>";
-    echo "<td><span class='ccolend'>".$row["end"]."</span></td>";
-      
-    echo "<td><span class='ccoldowun'>";
-    echo str_pad(decbin($row["dow"]), 7, "0", STR_PAD_LEFT);
-      
-    for ($i=1; $i<8; $i++) {
- //     echo $i;
-      }
-    echo "</span></td>";
-      
-    echo "<td><span class='ccolvalue'>".$row["value"]."</span></td>";
-    
-    echo "<td><form method='post' action='sched-edit.php?id=".$row["id"]."'>";
-    echo "<input type='submit' name='edit' value='Edit'></form></td>";
-    echo "<td><form method='post' action='sched-delete.php?id=".$row["id"]."'>";
-    echo "<input type='submit' name='delete' value='Delete'></form></td>";
-    echo "</tr>";
-  }    
-  
-  echo "</table>";
-  
-} else {
-    echo "0 results";
-}
+
   
 mysqli_close($conn);
 ?>  
