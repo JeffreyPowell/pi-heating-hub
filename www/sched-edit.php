0166b54372c64d944d4390e25313ce1bae023292
Jeff <Jffrypwll@googlemail.com>
2016-12-06 16:14:49 +0000

    Update sched-edit.php

--- www/sched-edit.php
+++ www/sched-edit.php
@@ -24,7 +24,7 @@ if (!$conn) {
 $sql = "SELECT * FROM schedules WHERE id=".$SCHED_ID;
 
 $result = mysqli_query($conn, $sql);
-if (mysqli_num_rows($result) = 0) {
+if (mysqli_num_rows($result) == 0) {
         echo "0 results"; 
     }
     
