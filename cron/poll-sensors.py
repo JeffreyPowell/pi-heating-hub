3000fcc56ec01ffcf32509e60ebbc61695e4dc9b
Jeff <Jffrypwll@googlemail.com>
2016-12-13 23:37:57 +0000

    Update poll-sensors.py

--- cron/poll-sensors.py
+++ cron/poll-sensors.py
@@ -37,6 +37,26 @@ for i in results:
   cursorwrite.execute("UPDATE sensors SET value='%s' WHERE id='%s';" % (data, sensor_id))
 
   cnx.commit()
+  
+  
+  filename = '/home/pi/pi-heating-hub/data/s-'+sensor_id+'.rrd'
+
+  if( not os.path.exists( filename ) ):
+      print ( os.path.exists( filename ))
+      os.system('/usr/bin/rrdtool create '+filename+' --step 60 \
+      --start now \
+      DS:data:GAUGE:120:U:U \
+      RRA:MIN:0.5:1:10080 \
+      RRA:MIN:0.5:5:51840 \
+      RRA:MIN:0.5:60:8760 \
+      RRA:AVERAGE:0.5:1:10080 \
+      RRA:AVERAGE:0.5:5:51840 \
+      RRA:AVERAGE:0.5:60:8760 \
+      RRA:MAX:0.5:1:10080 \
+      RRA:MAX:0.5:5:51840 \
+      RRA:MAX:0.5:60:8760')
+
+  os.system('/usr/bin/rrdtool update '+filename+" "+str(t)+':'+data)
 
 
 cnx.close()
