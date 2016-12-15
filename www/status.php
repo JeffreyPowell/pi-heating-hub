002b151dc9b95eaae33f79dbf45f82b75eebd885
Jeff <Jffrypwll@googlemail.com>
2016-12-15 21:19:35 +0000

    Update status.php

--- www/status.php
+++ www/status.php
@@ -54,11 +54,6 @@ while($row = mysqli_fetch_assoc($result)) {
     $SENSOR_VALUE = $row["value"];
     }
     
-echo $SENSOR_NAME ;
-    
-printf('%2.1f', $SENSOR_VALUE);
-sprintf('%2.1f', $SENSOR_VALUE);
-    
 echo '<br><br>';
     
     
@@ -66,13 +61,14 @@ echo '<br><br>';
 echo "<table width='100%' border='1'>";
 echo "<tr><td width=30%>1</td><td width=30%>";
 echo "<span class='sensorname'>".$SENSOR_NAME."</span><br>";
+
 echo "<span class='sensorvalue'>";
    
 printf('%2.1f', $SENSOR_VALUE);
 sprintf('%2.1f', $SENSOR_VALUE);
     
 echo "</span></td>";
-echo "<span class='sensorvalue'>".$SENSOR_VALUE."</span></td>";
+
 
 echo "<td width=30%>3</td></tr>";
 echo "<tr><td width=30%>4</td><td width=30%>5</td><td width=30%>6</td></tr>";
