a408c13dbc6b8323f851729010c23d1055936c0b
Jeff <Jffrypwll@googlemail.com>
2016-12-20 16:55:04 +0000

    Update status.php

--- www/status.php
+++ www/status.php
@@ -119,7 +119,7 @@ while($row = mysqli_fetch_assoc($result_modes)) {
     echo $MODE_VALUE;
     echo "<br>";
     echo "<form name='modes' method='post' action='status.php?sid=".$SENSOR_ID."&gid=".$GRAPH_ID."&gsp=".$GRAPH_SP."'>";
-    echo "<input type='submit' class='button' name='".$MODE_ID."' value='Enable ".$MODE_NAME." mode'></form>";
+    echo "Enable <input type='submit' class='button' name='enable-".$MODE_ID."' value='".$MODE_NAME."'> mode</form>";
     }
 
 
