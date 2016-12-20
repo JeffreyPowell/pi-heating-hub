92fa98e75fc03685450d544f87fd654d235a825d
Jeff <Jffrypwll@googlemail.com>
2016-12-20 20:23:26 +0000

    Update update-timers.py

--- cron/update-timers.py
+++ cron/update-timers.py
@@ -1,1 +1,24 @@
+#!/usr/bin/env python
 
+import MySQLdb
+import datetime
+import urllib2
+import os
+  
+servername = "localhost"
+username = "pi"
+password = "password"
+dbname = "pi_heating_db"
+
+cnx = MySQLdb.connect(host=servername, user=username, passwd=password, db=dbname)
+cursorread = cnx.cursor()
+
+query = ("UPDATE `timers` set value = value -1 WHERE value >0")
+
+cursorupdate.execute(query)
+  
+cursorupdate.close()
+ 
+cnx.commit()
+
+cnx.close()
