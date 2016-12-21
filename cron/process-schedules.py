fb15fa08d63a01c09a98aa4c6f320433f7c31f86
Jeff <Jffrypwll@googlemail.com>
2016-12-21 16:44:43 +0000

    Update process-schedules.py

--- cron/process-schedules.py
+++ cron/process-schedules.py
@@ -157,27 +157,27 @@ for result in results_schedules:
     results_timers =cursorselect.fetchall()
     cursorselect.close()
   
-    print( results_timers )
+    #print( results_timers )
   
     SCHED_TEST_TIMERS = True
    
     for result in results_timers:
       print( result )
-      #TIMER_VALUE= result[2]
-      #TIMER_TEST = result[6]
-      #TEST_VALUE = result[7]
+      TIMER_VALUE= result[4]
+      TIMER_TEST = result[8]
+      TEST_VALUE = result[9]
     
-      #if (  MODE_TEST == '=' and MODE_VALUE == TEST_VALUE ):
-      #  TEST = True
-      #elif( MODE_TEST == '!' and MODE_VALUE != TEST_VALUE ):
-      #  TEST = True
-      #else:
-      #  TEST = False
-     # 
-     # if TEST == False:
-     #   SCHED_TEST_MODES = False
- # 
- #     print( MODE_VALUE, MODE_TEST, TEST_VALUE, SCHED_TEST_MODES ) 
+      if (  MODE_TEST == '=' and MODE_VALUE == TEST_VALUE ):
+        TEST = True
+      elif( MODE_TEST == '!' and MODE_VALUE != TEST_VALUE ):
+        TEST = True
+      else:
+        TEST = False
+      
+      if TEST == False:
+        SCHED_TEST_MODES = False
+  
+      print( TIMER_VALUE, TIMER_TEST, TEST_VALUE, SCHED_TEST_TIMERS ) 
   
   
   
