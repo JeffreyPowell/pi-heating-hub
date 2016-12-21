5e369413fdabe1c9eea74bfd81bcbb6aadd76170
Jeff <Jffrypwll@googlemail.com>
2016-12-21 11:48:00 +0000

    Update process-schedules.py

--- cron/process-schedules.py
+++ cron/process-schedules.py
@@ -78,7 +78,7 @@ for result in results_schedules:
   print( MIN_TO_START )
   print( MIN_TO_END )
   
-  if ( MIN_TO_START > 0 and MIN_TO_END > 0 ):
+  if ( MIN_TO_START.total_seconds() > 0 and MIN_TO_END.total_seconds() > 0 ):
     SCHED_TEST_TIME = True
     
   print( SCHED_TEST_TIME )
