d43a151fd1326e2154c0e918625c82534c174880
Jeff <Jffrypwll@googlemail.com>
2016-12-22 18:40:44 +0000

    Update wrapper.sh

--- cron/wrapper.sh
+++ cron/wrapper.sh
@@ -1,1 +1,6 @@
 #!/bin/bash
+
+/usr/bin/python /home/pi/pi-heating-hub/cron/poll-sensors.py
+/usr/bin/python /home/pi/pi-heating-hub/cron/update-timers.py
+/usr/bin/python /home/pi/pi-heating-hub/cron/process-schedules.py
+/usr/bin/python /home/pi/pi-heating-hub/cron/activate-devices.py
