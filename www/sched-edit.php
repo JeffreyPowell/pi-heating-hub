a88f70a4fb04030e1251062f9ac69da08c0f347c
Jeff <Jffrypwll@googlemail.com>
2017-01-06 18:16:26 +0000

    Update sched-edit.php

--- www/sched-edit.php
+++ www/sched-edit.php
@@ -93,19 +93,20 @@ if ($_SERVER["REQUEST_METHOD"] == "POST" && $_POST["formSubmit"] == "Save" ) {
 
     foreach( $_POST as $key => $val )
     {
-        print_r( $key );
-        print_r( $val );
+        #print_r( $key );
+        #print_r( $val );
 
-        //If this is one of the item name variables
+        print_r("<pre><BR>==========<BR>" );
         if( preg_match( '/sensor*opp/', $key ) )
         {
-            print_r("<pre><BR>==========<BR>");
+            #print_r("<pre><BR>==========<BR>");
             print_r( $key );
             print_r( $val );
             $post_sched_sensor_sensor_id = explode( '_', $key )[1];
             print_r( $post_sched_sensor_sensor_id );
-            print_r("<BR>==========<BR></pre>");
+            #print_r("<BR>==========<BR></pre>");
         }
+        print_r("<BR>==========<BR></pre>");
         print_r("<BR>");
     }
     
