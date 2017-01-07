17cf4d5e817bef0ccb9e694317d59b0d88cf659e
Jeff <Jffrypwll@googlemail.com>
2017-01-07 19:42:59 +0000

    Update wrapper.sh

--- cron/wrapper.sh
+++ cron/wrapper.sh
@@ -2,5 +2,6 @@
 
 python /home/pi/pi-heating-hub/cron/poll-sensors.py
 python /home/pi/pi-heating-hub/cron/update-timers.py
+python /home/pi/pi-heating-hub/cron/scan-network.py
 python /home/pi/pi-heating-hub/cron/process-schedules.py
 python /home/pi/pi-heating-hub/cron/activate-devices.py
