dcdccb030884bd52db2628c8b4ac8edf1989a2db
Jeff <Jffrypwll@googlemail.com>
2017-01-08 19:00:42 +0000

    Update sensors-list.php

--- www/sensors-list.php
+++ www/sensors-list.php
@@ -47,7 +47,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
             die("Connection failed: " . mysqli_connect_error());
             }
 
-        $sql = "INSERT INTO sensors (ip) VALUES ('000.000.000.000')";
+        $sql = "INSERT INTO sensors (ip, ref) VALUES ('000.000.000.000', '0')";
 
         if (!mysqli_query($conn, $sql)) {
             echo "Error: " . $sql . "<br>" . mysqli_error($conn);
