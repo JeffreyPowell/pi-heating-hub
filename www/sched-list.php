56783161035e33ba8c71bdb2dcca59db7ccd7518
Jeff <Jffrypwll@googlemail.com>
2017-01-08 14:31:36 +0000

    Update sched-list.php

--- www/sched-list.php
+++ www/sched-list.php
@@ -107,12 +107,12 @@ if (mysqli_num_rows($result) > 0) {
         
         
         $SCHED_DOW1 = (bool)$row["dow1"]; if ( $SCHED_DOW1 ) { $SCHED_DOW1_CHK = 'checked="checked"'; }else{ $SCHED_DOW1_CHK = ''; }
-        $SCHED_DOW2 = bool($row["dow2"]); if ( $SCHED_DOW2 ) { $SCHED_DOW2_CHK = 'checked="checked"'; }else{ $SCHED_DOW2_CHK = ''; }
-        $SCHED_DOW3 = bool($row["dow3"]); if ( $SCHED_DOW3 ) { $SCHED_DOW3_CHK = 'checked="checked"'; }else{ $SCHED_DOW3_CHK = ''; }
-        $SCHED_DOW4 = bool($row["dow4"]); if ( $SCHED_DOW4 ) { $SCHED_DOW4_CHK = 'checked="checked"'; }else{ $SCHED_DOW4_CHK = ''; }
-        $SCHED_DOW5 = bool($row["dow5"]); if ( $SCHED_DOW5 ) { $SCHED_DOW5_CHK = 'checked="checked"'; }else{ $SCHED_DOW5_CHK = ''; }
-        $SCHED_DOW6 = bool($row["dow6"]); if ( $SCHED_DOW6 ) { $SCHED_DOW6_CHK = 'checked="checked"'; }else{ $SCHED_DOW6_CHK = ''; }
-        $SCHED_DOW7 = bool($row["dow7"]); if ( $SCHED_DOW7 ) { $SCHED_DOW7_CHK = 'checked="checked"'; }else{ $SCHED_DOW7_CHK = ''; }
+        $SCHED_DOW2 = (bool)$row["dow2"]; if ( $SCHED_DOW2 ) { $SCHED_DOW2_CHK = 'checked="checked"'; }else{ $SCHED_DOW2_CHK = ''; }
+        $SCHED_DOW3 = (bool)$row["dow3"]; if ( $SCHED_DOW3 ) { $SCHED_DOW3_CHK = 'checked="checked"'; }else{ $SCHED_DOW3_CHK = ''; }
+        $SCHED_DOW4 = (bool)$row["dow4"]; if ( $SCHED_DOW4 ) { $SCHED_DOW4_CHK = 'checked="checked"'; }else{ $SCHED_DOW4_CHK = ''; }
+        $SCHED_DOW5 = (bool)$row["dow5"]; if ( $SCHED_DOW5 ) { $SCHED_DOW5_CHK = 'checked="checked"'; }else{ $SCHED_DOW5_CHK = ''; }
+        $SCHED_DOW6 = (bool)$row["dow6"]; if ( $SCHED_DOW6 ) { $SCHED_DOW6_CHK = 'checked="checked"'; }else{ $SCHED_DOW6_CHK = ''; }
+        $SCHED_DOW7 = (bool)$row["dow7"]; if ( $SCHED_DOW7 ) { $SCHED_DOW7_CHK = 'checked="checked"'; }else{ $SCHED_DOW7_CHK = ''; }
 
         
         
