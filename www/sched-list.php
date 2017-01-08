acf28b2dd0200877a683fc29b31080ac42499529
Jeff <Jffrypwll@googlemail.com>
2017-01-08 00:12:54 +0000

    Update sched-list.php

--- www/sched-list.php
+++ www/sched-list.php
@@ -118,7 +118,7 @@ if (mysqli_num_rows($result) > 0) {
     echo "<input type='submit' name='edit' value='Edit'></form></td>";
     echo "<td><form method='post' action='/sched-list.php?id=".$row["id"]."'>";
     echo "<input type='hidden' name='sched_id' value='".$row["id"]."'>";
-    echo "<input type='submit' name='delete"' value='Delete'></form></td>";
+    echo "<input type='submit' name='delete' value='Delete'></form></td>";
     echo "</tr>";
   }    
   
