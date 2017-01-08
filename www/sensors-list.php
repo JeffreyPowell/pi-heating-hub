0604f171e81d983bb47c70375e06420ed315fb56
Jeff <Jffrypwll@googlemail.com>
2017-01-08 18:32:06 +0000

    Update sensors-list.php

--- www/sensors-list.php
+++ www/sensors-list.php
@@ -1,8 +1,16 @@
 <!DOCTYPE HTML>
-
-<html><head>
+<html>
+<head>
 <meta http-equiv="refresh" content="30">
-</head><body bgcolor='#080808'>
+<style>
+.error {color: #FF0000;}
+.tcolname {font-family: arial; color: black; font-size: xx-large;}
+.ccolname {font-family: arial; color: black; font-size: large;}
+.ccoldowun {font-family: courier; color: darkgrey; font-size: x-small;}
+.ccoldowse {font-family: courier; color: black; font-size: large;}
+</style>
+</head>
+<body bgcolor='#080808'>
 <font color='#808080' size ='4' face='verdana'>
     
 <?php
@@ -16,6 +24,65 @@ $username = "pi";
 $password = "password";
 $dbname = "pi_heating_db";
 
+    
+///////////////////////////////////////////////////////////
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    print_r("------------------------");
+    print_r($_POST);
+    print_r("------------------------");
+    print_r($_GET);
+    print_r("------------------------");
+    
+    if ( $_POST["done"] == "Done" ) {
+        header('Location: /status.php');
+        exit();
+    }
+   
+    if ( $_POST["add"] == "Add new" ) {
+        // Create connection
+        $conn = mysqli_connect($servername, $username, $password, $dbname);
+        // Check connection
+        if (!$conn) {
+            die("Connection failed: " . mysqli_connect_error());
+            }
+
+        $sql = "INSERT INTO sensors (ip) VALUES ('000.000.000.000')";
+
+        if (!mysqli_query($conn, $sql)) {
+            echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+        }
+
+        mysqli_close($conn);
+    }
+    
+    if ( $_POST["delete"] == "Delete" ) {
+    
+        // Create connection
+        $conn = mysqli_connect($servername, $username, $password, $dbname);
+        // Check connection
+        if (!$conn) {
+            die("Connection failed: " . mysqli_connect_error());
+            }
+        
+        $SENSOR_ID = $_POST["sensor_id"];
+        
+        #echo $SCHED_ID;
+        
+        $sql = "DELETE FROM sched_sensor WHERE sensor_id='".$SENSOR_ID."';";
+        if (!mysqli_query($conn, $sql)) {
+            echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+        }
+
+        $sql = "DELETE FROM sensors WHERE id='".$SENSOR_ID."';";
+        if (!mysqli_query($conn, $sql)) {
+            echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+        }
+
+        mysqli_close($conn);
+    }
+}
+///////////////////////////////////////////////////////////
     
 // Create connection
 $conn = mysqli_connect($servername, $username, $password, $dbname);
@@ -38,38 +105,42 @@ if (mysqli_num_rows($result) > 0) {
     $id = $row["id"];
     $name = $row["name"];
       
+    $SENSOR_ID = $row["id"];
+    $SENSOR_NAME = $row["name"
+      
     $img_dir = 'images/chart-sensor-';
     $rrd_dir = '/home/pi/pi-heating-hub/data/s-';
 
     echo '<tr>';
       
     echo '<td>';
-    echo "<form method='get' action='sensor-edit.php?id=".$id."'>";
+    echo "<form method='get' action='sensor-edit.php?id=".$SENSOR_ID."'>";
     echo "<input type='submit' name='edit' value='Edit'></form>";
     echo '</td>';
 
     echo '<td>';
-    echo "<form method='get action='sensor-delete.php?id=".$id."'>";
+    echo "<form method='post' action='sensor-list.php'>";
+    echo "<input type='hidden' name='sensor_id' value='".$SENSOR_ID."'>"
     echo "<input type='submit' name='delete' value='Delete'></form>";
     echo '</td>';
       
     echo '<td>';
-    $span = "-24h";
-    create_graph( $rrd_dir.$id.".rrd", $img_dir.$id.$span.".png", 	$span, 	$row["name"]." last 24 hours",	 	   "120", "500");
-    echo "<img src='".$img_dir.$id.$span.".png' alt='RRD image'>";
+    $span = "-12h";
+    create_graph( $rrd_dir.$SENSOR_ID.".rrd", $img_dir.$SENSOR_ID.$span.".png", 	$span, 	$row["name"]." last 12 hours",	 	   "120", "500");
+    echo "<img src='".$img_dir.$SENSOR_ID.$span.".png' alt='RRD image'>";
     echo '</td>';
       
-    echo '<td>';
-    $span = "-7d";
-    create_graph( $rrd_dir.$row["id"].".rrd", $img_dir.$id.$span.".png", 	$span, 	$row["name"]." last 7 days",	 	   "120", "300");
-    echo "<img src='".$img_dir.$id.$span.".png' alt='RRD image'>";
-    echo '</td>';
+    #echo '<td>';
+    #$span = "-7d";
+    #create_graph( $rrd_dir.$SENSOR_ID.".rrd", $img_dir.$SENSOR_ID.$span.".png", 	$span, 	$row["name"]." last 7 days",	 	   "120", "300");
+    #echo "<img src='".$img_dir.$SENSOR_ID.$span.".png' alt='RRD image'>";
+    #echo '</td>';
   
-    echo '<td>';
-    $span = "-90d";
-    create_graph( $rrd_dir.$id.".rrd", $img_dir.$id.$span.".png", 	$span, 	$row["name"]." last 3 months",	 	   "120", "200");
-    echo "<img src='".$img_dir.$id.$span.".png' alt='RRD image'>";
-    echo '</td>';
+    #echo '<td>';
+    #$span = "-90d";
+    #create_graph( $rrd_dir.$id.".rrd", $img_dir.$id.$span.".png", 	$span, 	$row["name"]." last 3 months",	 	   "120", "200");
+    #echo "<img src='".$img_dir.$id.$span.".png' alt='RRD image'>";
+    #echo '</td>';
       
     echo '</tr>';
 
@@ -130,6 +201,5 @@ function create_graph($rrdfile, $output, $start, $title, $height, $width) {
 ?>
 
 </font>
-</div>
 </body>
 </html>
