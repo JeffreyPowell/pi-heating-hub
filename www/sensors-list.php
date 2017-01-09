b01b48636397c7d91a6e09c575d3be7f17eec6f2
Jeff <Jffrypwll@googlemail.com>
2017-01-09 19:56:48 +0000

    Update sensors-list.php

--- www/sensors-list.php
+++ www/sensors-list.php
@@ -137,57 +137,57 @@ $sql = "SELECT * FROM sensors;";
 $result = mysqli_query($conn, $sql);
 if (mysqli_num_rows($result) > 0) {
 
-  echo '<table>';
+    echo '<table>';
 
-  while($row = mysqli_fetch_assoc($result)) {
+    while($row = mysqli_fetch_assoc($result)) {
       
-    #$id = $row["id"];
-    #$name = $row["name"];
+        #$id = $row["id"];
+        #$name = $row["name"];
+        $SENSOR_ID = $row["id"];
+        $SENSOR_NAME = $row["name"];
+        $SENSOR_VALUE = $row["value"];
+        $SENSOR_UNIT = $row["unit"];
       
-    $SENSOR_ID = $row["id"];
-    $SENSOR_NAME = $row["name"];
-      
-    $img_dir = 'images/chart-sensor-';
-    $rrd_dir = '/home/pi/pi-heating-hub/data/s-';
+        $img_dir = 'images/chart-sensor-';
+        $rrd_dir = '/home/pi/pi-heating-hub/data/s-';
 
-    echo '<tr>';
+        echo '<tr>';
       
-    echo '<td>';
-    echo "<form method='post' action='sensor-edit.php?id=".$SENSOR_ID."'>";
-    echo "<input type='submit' name='edit' value='Edit'></form>";
-    echo '</td>';
+        echo '<td>';
+        echo $SENSOR_VALUE." ".$SENSOR_UNIT;
+        echo "<input type='submit' name='edit' value='Edit'></form>";
+        echo '</td>';
 
-    echo '<td>';
-    echo "<form method='post' action='sensors-list.php'>";
-    echo "<input type='hidden' name='sensor_id' value='".$SENSOR_ID."'>";
-    echo "<input type='submit' name='delete' value='Delete'></form>";
-    echo '</td>';
+        echo '<td>';
+        echo "<form method='post' action='sensors-list.php'>";
+        echo "<input type='hidden' name='sensor_id' value='".$SENSOR_ID."'>";
+        echo "<input type='submit' name='delete' value='Delete'></form>";
+        echo '</td>';
       
-    echo '<td>';
-    $span = "-12h";
-    create_graph( $rrd_dir.$SENSOR_ID.".rrd", $img_dir.$SENSOR_ID.$span.".png", 	$span, 	$row["name"]." last 12 hours",	 	   "60", "400");
-    if ( file_exists( $img_dir.$SENSOR_ID.$span.".png") ){
-        echo "<img src='".$img_dir.$SENSOR_ID.$span.".png' alt='RRD image'>";
-    }
-    echo '</td>';
+        echo '<td>';
+        $span = "-12h";
+        create_graph( $rrd_dir.$SENSOR_ID.".rrd", $img_dir.$SENSOR_ID.$span.".png", 	$span, 	$row["name"]." last 12 hours",	 	   "60", "400");
+        if ( file_exists( $img_dir.$SENSOR_ID.$span.".png") ){
+            echo "<img src='".$img_dir.$SENSOR_ID.$span.".png' alt='RRD image'>";
+        }
+        echo '</td>';
       
-    #echo '<td>';
-    #$span = "-7d";
-    #create_graph( $rrd_dir.$SENSOR_ID.".rrd", $img_dir.$SENSOR_ID.$span.".png", 	$span, 	$row["name"]." last 7 days",	 	   "120", "300");
-    #echo "<img src='".$img_dir.$SENSOR_ID.$span.".png' alt='RRD image'>";
-    #echo '</td>';
+        #echo '<td>';
+        #$span = "-7d";
+        #create_graph( $rrd_dir.$SENSOR_ID.".rrd", $img_dir.$SENSOR_ID.$span.".png", 	$span, 	$row["name"]." last 7 days",	 	   "120", "300");
+        #echo "<img src='".$img_dir.$SENSOR_ID.$span.".png' alt='RRD image'>";
+        #echo '</td>';
   
-    #echo '<td>';
-    #$span = "-90d";
-    #create_graph( $rrd_dir.$id.".rrd", $img_dir.$id.$span.".png", 	$span, 	$row["name"]." last 3 months",	 	   "120", "200");
-    #echo "<img src='".$img_dir.$id.$span.".png' alt='RRD image'>";
-    #echo '</td>';
+        #echo '<td>';
+        #$span = "-90d";
+        #create_graph( $rrd_dir.$id.".rrd", $img_dir.$id.$span.".png", 	$span, 	$row["name"]." last 3 months",	 	   "120", "200");
+        #echo "<img src='".$img_dir.$id.$span.".png' alt='RRD image'>";
+        #echo '</td>';
       
-    echo '</tr>';
+        echo '</tr>';
+    }
 
-  }
-
-  echo "</table>";
+    echo "</table>";
     
 }
 
@@ -199,43 +199,43 @@ mysqli_close($conn);
 
 function create_graph($rrdfile, $output, $start, $title, $height, $width) {
     
-  $options = array(
-    "--slope-mode",
-    "--start", $start,
-    "--title=$title",
-    "--vertical-label=Temperature",
-#    "--lower=0",
-    "--height=$height",
-    "--width=$width",
-    "-cBACK#161616",
-    "-cCANVAS#1e1e1e",
-    "-cSHADEA#000000",
-    "-cSHADEB#000000",
-    "-cFONT#c7c7c7",
-    "-cGRID#888800",
-    "-cMGRID#ffffff",
-    "-nTITLE:10",
-    "-nAXIS:12",
-    "-nUNIT:10",
-    "-y 1:5",
-    "-cFRAME#ffffff",
-    "-cARROW#000000",
-    "DEF:callmax=$rrdfile:data:MAX",
-    "CDEF:transcalldatamax=callmax,1,*",
-    "AREA:transcalldatamax#a0b84240",
-    "LINE4:transcalldatamax#a0b842",
-#    "LINE4:transcalldatamax#a0b842:Calls",
-#    "COMMENT:\\n",
-#    "GPRINT:transcalldatamax:LAST:Calls Now %6.2lf",
-#    "GPRINT:transcalldatamax:MAX:Data %6.2lf"
-    "COMMENT:\\n"
-  );
+    $options = array(
+        "--slope-mode",
+        "--start", $start,
+        "--title=$title",
+        "--vertical-label=Temperature",
+#        "--lower=0",
+        "--height=$height",
+        "--width=$width",
+        "-cBACK#161616",
+        "-cCANVAS#1e1e1e",
+        "-cSHADEA#000000",
+        "-cSHADEB#000000",
+        "-cFONT#c7c7c7",
+        "-cGRID#888800",
+        "-cMGRID#ffffff",
+        "-nTITLE:10",
+        "-nAXIS:12",
+        "-nUNIT:10",
+        "-y 1:5",
+        "-cFRAME#ffffff",
+        "-cARROW#000000",
+        "DEF:callmax=$rrdfile:data:MAX",
+        "CDEF:transcalldatamax=callmax,1,*",
+        "AREA:transcalldatamax#a0b84240",
+        "LINE4:transcalldatamax#a0b842",
+#        "LINE4:transcalldatamax#a0b842:Calls",
+#        "COMMENT:\\n",
+#        "GPRINT:transcalldatamax:LAST:Calls Now %6.2lf",
+#        "GPRINT:transcalldatamax:MAX:Data %6.2lf"
+        "COMMENT:\\n"
+    );
 
- $ret = rrd_graph( $output, $options );
+    $ret = rrd_graph( $output, $options );
 
-  if (! $ret) {
-    echo "<b>Waiting for initial data</b>";
-  }
+    if (! $ret) {
+        echo "<b>Waiting for initial data</b>";
+    }
 }
 
 
