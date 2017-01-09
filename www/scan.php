a2d7644f776457619d0962ba4e89af1559d68811
Jeff <Jffrypwll@googlemail.com>
2017-01-09 13:28:48 +0000

    Create scan.php

---
+++ www/scan.php
@@ -0,0 +1,27 @@
+
+<!DOCTYPE HTML>
+<html>
+<head>
+<style>
+.fixedsmall {font-family: courier; color: black; font-size: xx-small;}
+</style>
+</head>
+<body class='fixedsmall'>
+
+<?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+$servername = "localhost";
+$username = "pi";
+$password = "password";
+$dbname = "pi_heating_db";
+$SCHED_ID = $_GET['id'];
+
+
+
+mysqli_close($conn);
+?>
+
+</body>
+</html>
