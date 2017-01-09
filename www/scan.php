683e460fd900b61316ed38ad20b5c692764005dc
Jeff <Jffrypwll@googlemail.com>
2017-01-09 16:19:19 +0000

    Update scan.php

--- www/scan.php
+++ www/scan.php
@@ -47,12 +47,13 @@
             $sensor_count = '0';
         }
         
+        echo $sensor_count;
+        echo '<br>';
+        
         set_error_handler(function() { $sensor_count = '0'; });
         $sensor_count = file_get_contents("http://".$device_ip.":8080/count.php");
         restore_error_handler();
-        
-        
-        
+
         echo $sensor_count;
         echo '<br>';
     }
