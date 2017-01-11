88909e333e687f628d07d56b95ef3258641c39b2
Jeff <Jffrypwll@googlemail.com>
2017-01-11 13:07:47 +0000

    Update poll-sensors.py

--- cron/poll-sensors.py
+++ cron/poll-sensors.py
@@ -33,52 +33,48 @@ for i in results:
   try:
     data = float( urllib2.urlopen(sensor_url).read() )  
   except:
-    data = 'na'
+    data = 'null'
     
   print data
   print sensor_id
-  
-  if( data != 'na' ):
-    print "database"
-    #print sensor_id
-    
-    sql = "UPDATE sensors SET value='"+str(data)+"' WHERE id='"+str(sensor_id)+"';"
+   
+  sql = "UPDATE sensors SET value='"+str(data)+"' WHERE id='"+str(sensor_id)+"';"
 
-    print sql
+  print sql
        
-    #cursorwrite.execute( sql )
+  #cursorwrite.execute( sql )
     
+  try:
+    cnx = MySQLdb.connect(host=servername, user=username, passwd=password, db=dbname)
+    cnx.autocommit(True)
+    cursorwrite = cnx.cursor()
+    cursorwrite.execute( sql )
+      
+    print("affected rows = {}".format(cursorwrite.rowcount))
+      
+    cursorwrite.close()
+    cnx.close()
+
+    #rows = cur.fetchall()
+  except MySQLdb.Error, e:
     try:
-      cnx = MySQLdb.connect(host=servername, user=username, passwd=password, db=dbname)
-      cnx.autocommit(True)
-      cursorwrite = cnx.cursor()
-      cursorwrite.execute( sql )
-      
-      print("affected rows = {}".format(cursorwrite.rowcount))
-      
-      cursorwrite.close()
-      cnx.close()
-
-      #rows = cur.fetchall()
-    except MySQLdb.Error, e:
-      try:
-        print "MySQL Error [%d]: %s" % (e.args[0], e.args[1])
-      except IndexError:
-        print "MySQL Error: %s" % str(e)
+      print "MySQL Error [%d]: %s" % (e.args[0], e.args[1])
+    except IndexError:
+      print "MySQL Error: %s" % str(e)
     
 
-    print "database done"
+  print "database done"
   
-  filename = '/home/pi/pi-heating-hub/data/s-'+str(sensor_id)+'.rrd'
+filename = '/home/pi/pi-heating-hub/data/s-'+str(sensor_id)+'.rrd'
   
-  print filename
+print filename
 
-  if( not os.path.exists( filename ) ):
-      print ( os.path.exists( filename ))
-      os.system('/usr/bin/rrdtool create '+filename+' --step 60 \
-      --start now \
-      DS:data:GAUGE:120:U:U \
-      RRA:MIN:0.5:1:10080 \
+if( not os.path.exists( filename ) ):
+    print ( os.path.exists( filename ))
+    os.system('/usr/bin/rrdtool create '+filename+' --step 60 \
+    --start now \
+    DS:data:GAUGE:120:U:U \
+    RRA:MIN:0.5:1:10080 \
       RRA:MIN:0.5:5:51840 \
       RRA:MIN:0.5:60:8760 \
       RRA:AVERAGE:0.5:1:10080 \
@@ -88,7 +84,7 @@ for i in results:
       RRA:MAX:0.5:5:51840 \
       RRA:MAX:0.5:60:8760')
 
-  if( data != 'na' ):
+  if( data != 'null' ):
     print"rrd"
     os.system('/usr/bin/rrdtool update '+filename+" "+str(t)+':'+str(data))
 
