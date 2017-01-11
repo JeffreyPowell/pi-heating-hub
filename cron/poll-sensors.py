7b0c3766c89492f7ba585aef62bb89f98b70ad09
Jeff <Jffrypwll@googlemail.com>
2017-01-11 13:59:15 +0000

    Update poll-sensors.py

--- cron/poll-sensors.py
+++ cron/poll-sensors.py
@@ -66,27 +66,26 @@ for i in results:
 
   print "database done"
   
-filename = '/home/pi/pi-heating-hub/data/s-'+str(sensor_id)+'.rrd'
+  filename = '/home/pi/pi-heating-hub/data/s-'+str(sensor_id)+'.rrd'
   
-print filename
+  print filename
 
-if( not os.path.exists( filename ) ):
-    print ( os.path.exists( filename ))
-    os.system('/usr/bin/rrdtool create '+filename+' --step 60 \
-    --start now \
-    DS:data:GAUGE:120:U:U \
-    RRA:MIN:0.5:1:10080 \
-    RRA:MIN:0.5:5:51840 \
-    RRA:MIN:0.5:60:8760 \
-    RRA:AVERAGE:0.5:1:10080 \
-    RRA:AVERAGE:0.5:5:51840 \
-    RRA:AVERAGE:0.5:60:8760 \
-    RRA:MAX:0.5:1:10080 \
-    RRA:MAX:0.5:5:51840 \
-    RRA:MAX:0.5:60:8760')
+  if( not os.path.exists( filename ) ):
+      print ( os.path.exists( filename ))
+      os.system('/usr/bin/rrdtool create '+filename+' --step 60 \
+      --start now \
+      DS:data:GAUGE:120:U:U \
+      RRA:MIN:0.5:1:10080 \
+      RRA:MIN:0.5:5:51840 \
+      RRA:MIN:0.5:60:8760 \
+      RRA:AVERAGE:0.5:1:10080 \
+      RRA:AVERAGE:0.5:5:51840 \
+      RRA:AVERAGE:0.5:60:8760 \
+      RRA:MAX:0.5:1:10080 \
+      RRA:MAX:0.5:5:51840 \
+      RRA:MAX:0.5:60:8760')
 
-if( data != 'NULL' ):
-  print"rrd"
-  os.system('/usr/bin/rrdtool update '+filename+" "+str(t)+':'+str(data))
-
+  if( data != 'NULL' ):
+    print"rrd"
+    os.system('/usr/bin/rrdtool update '+filename+" "+str(t)+':'+str(data))
 
