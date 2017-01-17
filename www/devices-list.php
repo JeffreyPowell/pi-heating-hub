1c55b6b558127c42f599617113ba0b084b23282f
Jeff <Jffrypwll@googlemail.com>
2017-01-17 12:29:37 +0000

    Update devices-list.php

--- www/devices-list.php
+++ www/devices-list.php
@@ -4,19 +4,20 @@
 <meta http-equiv="refresh" content="30">
 <style>
         .pbody { background-color: #080808; }
-        .debug { font-family: courier; color: red; font-size: large;}
-        .error { color: #FF0000;}
-        .ttab  { width: 90%;}
-        .tcol  { font: 22px arial;}
-        .tspan { font: 22px arial; color: grey;}
-        .dcol  { text-align: center; margin: 0 24px 0 0;}
-        .dspan { font-family: arial; color: grey; font-size: large; padding: 0 24px 0 0; display: inline-block;}
-        .ptitle { font: bold 32px arial; color: blue;}
-        .itextbox { font-family: arial; color: grey; font-size: large; padding: 12px 20px; margin: 8px 30px; width: 80%;}
-        .bgrey {  background-color: grey;  border: none; color: white; padding: 8px 16px; text-align: center; text-decoration: none; display: inline-block; font-size: 16px; font-family: arial; margin: 12px ;}
-        .bblue {  background-color: blue;  border: none; color: white; padding: 8px 16px; text-align: center; text-decoration: none; display: inline-block; font-size: 16px; font-family: arial; margin: 12px ;}
-        .bgreen { background-color: green; border: none; color: white; padding: 8px 16px; text-align: center; text-decoration: none; display: inline-block; font-size: 16px; font-family: arial; margin: 12px ;}
-        .bred {   background-color: red;   border: none; color: white; padding: 8px 16px; text-align: center; text-decoration: none; display: inline-block; font-size: 16px; font-family: arial; margin: 12px ;}
+        .debug { font-family: courier; color: red; font-size: large; }
+        .error { color: #FF0000; }
+        .ttab  { width: 90%; }
+        .tcol  { font: 22px arial; }
+        .tspan { font: 22px arial; color: grey; }
+        .dcolname   { text-align: left; margin: 0 24px 0 0; }
+        .dcolstatus { text-align: center; }
+        .dspan { font-family: arial; color: grey; font-size: large; padding: 0 24px 0 0; display: inline-block; }
+        .ptitle { font: bold 32px arial; color: blue; }
+        .itextbox { font-family: arial; color: grey; font-size: large; padding: 12px 20px; margin: 8px 30px; width: 80%; }
+        .bgrey {  background-color: grey;  border: none; color: white; padding: 8px 16px; text-align: center; text-decoration: none; display: inline-block; font-size: 16px; font-family: arial; margin: 12px ; }
+        .bblue {  background-color: blue;  border: none; color: white; padding: 8px 16px; text-align: center; text-decoration: none; display: inline-block; font-size: 16px; font-family: arial; margin: 12px ; }
+        .bgreen { background-color: green; border: none; color: white; padding: 8px 16px; text-align: center; text-decoration: none; display: inline-block; font-size: 16px; font-family: arial; margin: 12px ; }
+        .bred {   background-color: red;   border: none; color: white; padding: 8px 16px; text-align: center; text-decoration: none; display: inline-block; font-size: 16px; font-family: arial; margin: 12px ; }
         table, th, td { border: 5px solid #080808; }
         th, td {  background-color: #1a1a1a; }
         
