deca68d9a6a53b279523f759e949061e0581614a
Jeff <Jffrypwll@googlemail.com>
2017-01-18 16:15:12 +0000

    Update sched-edit.php

--- www/sched-edit.php
+++ www/sched-edit.php
@@ -7,15 +7,15 @@
     .error { color: #FF0000; }
     .ttab  { width: 100%; vertical-align: top; }
     .ttabsub  { width: 100%; border: 0px; text-align: center; font-family: arial; color: grey; font-size: small; }
-    .tcol  { font: 22px arial; }
-    .tspan { font: 22px arial; color: grey; margin: 16px; display: inline-block; }
+    .tcol  { font: 22pt arial; }
+    .tspan { font: 22pt arial; color: grey; margin: 16px; display: inline-block; }
     .dcolname   { text-align: left; padding: 8px 8px 8px 32px; }
     .dcolstatus { text-align: center; }
     .dspan { font-family: arial; color: grey; font-size: large; display: inline-block; }
     .ptitle { font: bold 32px arial; color: blue; }
     .ptitlesub { font: bold 24px arial; color: navy; }
     .itextbox { font-family: arial; color: grey; font-size: large; padding: 16px; margin: 16px; display: inline-block; }
-    .itextboxsub { font-family: arial; color: grey; font-size: small; padding: 8px; margin: 0px; display: inline-block; }
+    .itextboxsub { font-family: arial; color: grey; font-size: 12pt; padding: 8px; margin: 0px; display: inline-block; }
     .bgrey {  background-color: grey;  border: none; color: white; padding: 8px 16px; text-align: center; text-decoration: none; display: inline-block; font-size: 16px; font-family: arial; margin: 12px ; }
     .bblue {  background-color: blue;  border: none; color: white; padding: 8px 16px; text-align: center; text-decoration: none; display: inline-block; font-size: 16px; font-family: arial; margin: 12px ; }
     .bgreen { background-color: green; border: none; color: white; padding: 8px 16px; text-align: center; text-decoration: none; display: inline-block; font-size: 16px; font-family: arial; margin: 12px ; }
