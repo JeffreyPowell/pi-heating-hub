1ccb3d2b364b99288472ecb0f79c12553c1c47b7
Jeff <Jffrypwll@googlemail.com>
2017-01-18 12:02:46 +0000

    Update netdevice-edit.php

--- www/netdevice-edit.php
+++ www/netdevice-edit.php
@@ -53,7 +53,7 @@
                 if ( isset($_POST["save"]) ) {
                         #echo "#### save ####";
                         
-                        $POST_NETDEV_NAME = htmlentities($_POST["name"]);
+                        $POST_NETDEV_NAME = urlencode($_POST["name"]);
                         $POST_NETDEV_MAC = $_POST["mac"];
                         
                         // Create connection
@@ -91,7 +91,7 @@
         }
         
         $row = mysqli_fetch_assoc($result);
-        $NETDEV_NAME = urlencode($row["name"]);
+        $NETDEV_NAME = htmlentities($row["name"]);
         $NETDEV_MAC = $row["mac"];
         
         echo "<span class='ptitle'>EDIT Network Device '$NETDEV_NAME'</span><br><br>";
