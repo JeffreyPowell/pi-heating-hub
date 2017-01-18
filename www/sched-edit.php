33c60d4ea06d56ae2eaa1d3ebc87e5be3ef80802
Jeff <Jffrypwll@googlemail.com>
2017-01-18 14:41:02 +0000

    Update sched-edit.php

--- www/sched-edit.php
+++ www/sched-edit.php
@@ -8,7 +8,7 @@
     .ttab  { width: 100%; }
     .tcol  { font: 22px arial; }
     .tspan { font: 22px arial; color: grey; margin: 16px; display: inline-block; }
-    .dcolname   { text-align: left; padding: 0 0 0 32px; }
+    .dcolname   { text-align: left; padding: 8px 8px 8px 32px; }
     .dcolstatus { text-align: center; }
     .dspan { font-family: arial; color: grey; font-size: large; display: inline-block; }
     .ptitle { font: bold 32px arial; color: blue; }
