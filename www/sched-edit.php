f11861da899657235efdf4d87118e932737f531b
Jeff <Jffrypwll@googlemail.com>
2017-01-18 17:55:15 +0000

    Update sched-edit.php

--- www/sched-edit.php
+++ www/sched-edit.php
@@ -15,7 +15,7 @@
     .ptitle { font: bold 32px arial; color: blue; }
     .ptitlesub { font: bold 24px arial; color: navy; }
     .itextbox { font-family: arial; color: grey; font-size: 12pt; padding: 8px; margin: 4px; display: inline-block; }
-    .itextboxsub { font-family: arial; color: grey; font-size: 11pt; padding: 4px; margin: 0px; display: inline-block; }
+    .itextboxsub { font-family: arial; color: grey; font-size: 11pt; padding: 4px; margin: 0px; display: inline-block; width: auto; }
     .bgrey {  background-color: grey;  border: none; color: white; padding: 8px 16px; text-align: center; text-decoration: none; display: inline-block; font-size: 16px; font-family: arial; margin: 12px ; }
     .bblue {  background-color: blue;  border: none; color: white; padding: 8px 16px; text-align: center; text-decoration: none; display: inline-block; font-size: 16px; font-family: arial; margin: 12px ; }
     .bgreen { background-color: green; border: none; color: white; padding: 8px 16px; text-align: center; text-decoration: none; display: inline-block; font-size: 16px; font-family: arial; margin: 12px ; }
