c33267d4d10b9f5d7b3613616a5a7b8e5b0331c7
Jeff <Jffrypwll@googlemail.com>
2017-01-23 12:47:44 +0000

    Update mode-edit.php

--- www/mode-edit.php
+++ www/mode-edit.php
@@ -24,94 +24,95 @@
 <body class='pbody'>
 
 <?php
-        ini_set('display_errors', 1);
-        ini_set('display_startup_errors', 1);
-        error_reporting(E_ALL);
+    ini_set('display_errors', 1);
+    ini_set('display_startup_errors', 1);
+    error_reporting(E_ALL);
         
-        $servername = "localhost";
-        $username = "pi";
-        $password = "password";
-        $dbname = "pi_heating_db";
+    $ini_array = parse_ini_file("/home/pi/pi-heating-hub/config/config.ini", true);
+    
+    $servername = $ini_array['db']['server'];
+    $username =$ini_array['db']['user'];
+    $password = $ini_array['db']['password'];
+    $dbname = $ini_array['db']['database'];
 
 
-        $MODE_ID = $_GET['id'];
-        
-        #echo $DEVICE_ID;
+    $MODE_ID = $_GET['id'];
 
-        #echo $_SERVER["REQUEST_METHOD"];
-        
-        if ($_SERVER["REQUEST_METHOD"] == "POST" ) {
-                #print_r("<pre><BR>------------------------<BR>");
-                #print_r($_POST);
-                #print_r("<BR>------------------------<BR></pre>");
-                
-                #if ( isset($_POST["done"]) ) {
-                #        #echo "#### done ####";
-                #        header('Location: /devices-list.php');
-                #        exit();
-                #}
-                
-                if ( isset($_POST["save"]) ) {
-                        #echo "#### save ####";
-                        
-                        $POST_MODE_NAME = $_POST["name"];
-                        
-                        // Create connection
-                        $conn = mysqli_connect($servername, $username, $password, $dbname);
-                        // Check connection
-                        if (!$conn) {
-                                die("<br><br>Connection failed: " . mysqli_connect_error());
-                        }
-                        # Update schedules with post data
-                        $sql = "UPDATE modes SET name = '$POST_MODE_NAME' WHERE id='$MODE_ID';";
-                        #echo $sql;
-                        if (mysqli_query($conn, $sql)) {
-                                #echo "<br><br>Schedule updated successfully";
-                        } else {
-                                echo "<br><br>Error: " . $sql . "<br>" . mysqli_error($conn);
-                        }
-                        mysqli_close($conn);
-                }
-                
-        }
+    #echo $DEVICE_ID;
 
-        
-        // Create connection
-        $conn = mysqli_connect($servername, $username, $password, $dbname);
-        // Check connection
-        if (!$conn) {
+    #echo $_SERVER["REQUEST_METHOD"];
+
+    if ($_SERVER["REQUEST_METHOD"] == "POST" ) {
+        #print_r("<pre><BR>------------------------<BR>");
+        #print_r($_POST);
+        #print_r("<BR>------------------------<BR></pre>");
+
+        #if ( isset($_POST["done"]) ) {
+        #        #echo "#### done ####";
+        #        header('Location: /devices-list.php');
+        #        exit();
+        #}
+
+        if ( isset($_POST["save"]) ) {
+            #echo "#### save ####";
+
+            $POST_MODE_NAME = $_POST["name"];
+
+            // Create connection
+            $conn = mysqli_connect($servername, $username, $password, $dbname);
+            // Check connection
+            if (!$conn) {
                 die("<br><br>Connection failed: " . mysqli_connect_error());
+            }
+            # Update schedules with post data
+            $sql = "UPDATE modes SET name = '$POST_MODE_NAME' WHERE id='$MODE_ID';";
+            #echo $sql;
+            if (mysqli_query($conn, $sql)) {
+                #echo "<br><br>Schedule updated successfully";
+            } else {
+                echo "<br><br>Error: " . $sql . "<br>" . mysqli_error($conn);
+            }
+            mysqli_close($conn);
         }
-        echo '<form method="post" action="'.htmlspecialchars($_SERVER["PHP_SELF"]).'?id='.$MODE_ID.'">';
-        $sql = "SELECT * FROM modes WHERE id=".$MODE_ID;
-        #echo $sql;
-        $result = mysqli_query($conn, $sql);
-        #print_r( $result );
-        if (mysqli_num_rows($result) == 0) {
-                echo "0 results";
-        }
-        
-        $row = mysqli_fetch_assoc($result);
-        $MODE_NAME = $row["name"];
-        
-        echo "<span class='ptitle'>EDIT Mode '$MODE_NAME'</span><br><br>";
+    }
 
-        echo "<table class='ttab'>";
-        echo "<tr><td>";
 
-        echo "<span class='tspan'>Name</span><br>";
-        echo "<input type='text' name='name' value='$MODE_NAME' class='itextbox'><br><br>";
+    // Create connection
+    $conn = mysqli_connect($servername, $username, $password, $dbname);
+    // Check connection
+    if (!$conn) {
+        die("<br><br>Connection failed: " . mysqli_connect_error());
+    }
+    echo '<form method="post" action="'.htmlspecialchars($_SERVER["PHP_SELF"]).'?id='.$MODE_ID.'">';
+    $sql = "SELECT * FROM modes WHERE id=".$MODE_ID;
+    #echo $sql;
+    $result = mysqli_query($conn, $sql);
+    #print_r( $result );
+    if (mysqli_num_rows($result) == 0) {
+        echo "0 results";
+    }
 
-        echo "</td></tr>";
-        
-        echo "</table>";
-        
-        echo "<input type='submit' name='save' value='Save' class='bgreen' />";
-        echo "&nbsp;&nbsp;";
-        echo "<input type='button' onclick='location.href=\"/modes-list.php\";' value='Done' class='bgrey' />";
-        echo '</form>';
+    $row = mysqli_fetch_assoc($result);
+    $MODE_NAME = $row["name"];
 
-        mysqli_close($conn);
+    echo "<span class='ptitle'>EDIT Mode '$MODE_NAME'</span><br><br>";
+
+    echo "<table class='ttab'>";
+    echo "<tr><td>";
+
+    echo "<span class='tspan'>Name</span><br>";
+    echo "<input type='text' name='name' value='$MODE_NAME' class='itextbox'><br><br>";
+
+    echo "</td></tr>";
+
+    echo "</table>";
+
+    echo "<input type='submit' name='save' value='Save' class='bgreen' />";
+    echo "&nbsp;&nbsp;";
+    echo "<input type='button' onclick='location.href=\"/modes-list.php\";' value='Done' class='bgrey' />";
+    echo '</form>';
+
+    mysqli_close($conn);
 ?>
 
 </body>
