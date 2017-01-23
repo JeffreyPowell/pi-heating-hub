b5bf7d6cbf5e8bd3a2a04adf213b9ed20575444c
Jeff <Jffrypwll@googlemail.com>
2017-01-23 12:44:36 +0000

    Update device-edit.php

--- www/device-edit.php
+++ www/device-edit.php
@@ -24,103 +24,104 @@
 <body class='pbody'>
 
 <?php
-        ini_set('display_errors', 1);
-        ini_set('display_startup_errors', 1);
-        error_reporting(E_ALL);
+    ini_set('display_errors', 1);
+    ini_set('display_startup_errors', 1);
+    error_reporting(E_ALL);
         
-        $servername = "localhost";
-        $username = "pi";
-        $password = "password";
-        $dbname = "pi_heating_db";
+    $ini_array = parse_ini_file("/home/pi/pi-heating-hub/config/config.ini", true);
+    
+    $servername = $ini_array['db']['server'];
+    $username =$ini_array['db']['user'];
+    $password = $ini_array['db']['password'];
+    $dbname = $ini_array['db']['database'];
         
-        #print_r($_GET);
-        $DEVICE_ID = $_GET['id'];
+    #print_r($_GET);
+    $DEVICE_ID = $_GET['id'];
         
         #echo $DEVICE_ID;
         #echo $_SERVER["REQUEST_METHOD"];
         
-        if ($_SERVER["REQUEST_METHOD"] == "POST" ) {
-                #print_r("<pre><BR>------------------------<BR>");
-                #print_r($_POST);
-                #print_r("<BR>------------------------<BR></pre>");
+    if ($_SERVER["REQUEST_METHOD"] == "POST" ) {
+        #print_r("<pre><BR>------------------------<BR>");
+        #print_r($_POST);
+        #print_r("<BR>------------------------<BR></pre>");
                 
-                if ( isset($_POST["done"]) ) {
-                        #echo "#### done ####";
-                        header('Location: /devices-list.php');
-                        exit();
-                }
-                
-                if ( isset($_POST["save"]) ) {
-                        #echo "#### save ####";
-                        
-                        $POST_DEVICE_NAME = $_POST["name"];
-                        $POST_DEVICE_PIN = $_POST["pin"];
-                        $POST_DEVICE_ACTIVE_LEVEL = $_POST["active_level"];
-                        
-                        // Create connection
-                        $conn = mysqli_connect($servername, $username, $password, $dbname);
-                        // Check connection
-                        if (!$conn) {
-                                die("<br><br>Connection failed: " . mysqli_connect_error());
-                        }
-                        # Update schedules with post data
-                        $sql = "UPDATE devices SET name = '$POST_DEVICE_NAME', pin = '$POST_DEVICE_PIN', active_level = '$POST_DEVICE_ACTIVE_LEVEL' WHERE d_id='".$DEVICE_ID."';";
-                        #echo $sql;
-                        if (mysqli_query($conn, $sql)) {
-                                #echo "<br><br>Schedule updated successfully";
-                        } else {
-                                echo "<br><br>Error: " . $sql . "<br>" . mysqli_error($conn);
-                        }
-                        mysqli_close($conn);
-                }
-                
+        if ( isset($_POST["done"]) ) {
+            #echo "#### done ####";
+            header('Location: /devices-list.php');
+            exit();
         }
-        
-        // Create connection
-        $conn = mysqli_connect($servername, $username, $password, $dbname);
-        // Check connection
-        if (!$conn) {
+                
+        if ( isset($_POST["save"]) ) {
+            #echo "#### save ####";
+
+            $POST_DEVICE_NAME = $_POST["name"];
+            $POST_DEVICE_PIN = $_POST["pin"];
+            $POST_DEVICE_ACTIVE_LEVEL = $_POST["active_level"];
+
+            // Create connection
+            $conn = mysqli_connect($servername, $username, $password, $dbname);
+            // Check connection
+            if (!$conn) {
                 die("<br><br>Connection failed: " . mysqli_connect_error());
+            }
+            # Update schedules with post data
+            $sql = "UPDATE devices SET name = '$POST_DEVICE_NAME', pin = '$POST_DEVICE_PIN', active_level = '$POST_DEVICE_ACTIVE_LEVEL' WHERE d_id='".$DEVICE_ID."';";
+            #echo $sql;
+            if (mysqli_query($conn, $sql)) {
+                    #echo "<br><br>Schedule updated successfully";
+            } else {
+                echo "<br><br>Error: " . $sql . "<br>" . mysqli_error($conn);
+            }
+            mysqli_close($conn);
         }
-        echo '<form method="post" action="'.htmlspecialchars($_SERVER["PHP_SELF"]).'?id='.$DEVICE_ID.'">';
-        $sql = "SELECT * FROM devices WHERE d_id=".$DEVICE_ID;
-        #echo $sql;
-        $result = mysqli_query($conn, $sql);
-        #print_r( $result );
-        if (mysqli_num_rows($result) == 0) {
-                echo "0 results";
-        }
+    }
         
-        $row = mysqli_fetch_assoc($result);
-        $DEVICE_NAME = $row["name"];
-        $DEVICE_PIN = $row["pin"];
-        $DEVICE_ACTIVE_LEVEL = $row["active_level"];
+    // Create connection
+    $conn = mysqli_connect($servername, $username, $password, $dbname);
+    // Check connection
+    if (!$conn) {
+            die("<br><br>Connection failed: " . mysqli_connect_error());
+    }
+    echo '<form method="post" action="'.htmlspecialchars($_SERVER["PHP_SELF"]).'?id='.$DEVICE_ID.'">';
+    $sql = "SELECT * FROM devices WHERE d_id=".$DEVICE_ID;
+    #echo $sql;
+    $result = mysqli_query($conn, $sql);
+    #print_r( $result );
+    if (mysqli_num_rows($result) == 0) {
+            echo "0 results";
+    }
         
-        echo "<span class='ptitle'>EDIT Input Device '$DEVICE_NAME'</span><br><br>";
-        echo "<table class='ttab'>";
-        echo "<tr><td>";
-        echo "<span class='tspan'>Name</span><br>";
-        echo "<input type='text' name='name' value='$DEVICE_NAME' class='itextbox'><br><br>";
-        
-        echo "</td></tr><tr><td>";
-        
-        echo "<span class='tspan'>GPIO Pin</span><br>";
-        echo "<input type='text' name='pin' value='$DEVICE_PIN' class='itextbox'><br><br>";
-        
-        echo "</td></tr><tr><td>";
-        
-        echo "<span class='tspan'>Pin Active H/L</span><br>";
-        echo "<input type='text' name='active_level' value='$DEVICE_ACTIVE_LEVEL' class='itextbox'><br><br>";
- 
-        echo "</td></tr>";
-        
-        echo "</table>";
-        
-        echo "<input type='submit' name='save' value='Save' class='bgreen' />";
-        echo "&nbsp;&nbsp;";
-        echo "<input type='submit' name='done' value='Done' class='bgrey'  />";
-        echo '</form>';
-        mysqli_close($conn);
+    $row = mysqli_fetch_assoc($result);
+    $DEVICE_NAME = $row["name"];
+    $DEVICE_PIN = $row["pin"];
+    $DEVICE_ACTIVE_LEVEL = $row["active_level"];
+
+    echo "<span class='ptitle'>EDIT Input Device '$DEVICE_NAME'</span><br><br>";
+    echo "<table class='ttab'>";
+    echo "<tr><td>";
+    echo "<span class='tspan'>Name</span><br>";
+    echo "<input type='text' name='name' value='$DEVICE_NAME' class='itextbox'><br><br>";
+
+    echo "</td></tr><tr><td>";
+
+    echo "<span class='tspan'>GPIO Pin</span><br>";
+    echo "<input type='text' name='pin' value='$DEVICE_PIN' class='itextbox'><br><br>";
+
+    echo "</td></tr><tr><td>";
+
+    echo "<span class='tspan'>Pin Active H/L</span><br>";
+    echo "<input type='text' name='active_level' value='$DEVICE_ACTIVE_LEVEL' class='itextbox'><br><br>";
+
+    echo "</td></tr>";
+
+    echo "</table>";
+
+    echo "<input type='submit' name='save' value='Save' class='bgreen' />";
+    echo "&nbsp;&nbsp;";
+    echo "<input type='submit' name='done' value='Done' class='bgrey'  />";
+    echo '</form>';
+    mysqli_close($conn);
 ?>
 
 </body>
