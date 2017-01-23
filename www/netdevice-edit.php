087e858f8d55d53943bf75cf499efa27d097558a
Jeff <Jffrypwll@googlemail.com>
2017-01-23 12:52:50 +0000

    Update netdevice-edit.php

--- www/netdevice-edit.php
+++ www/netdevice-edit.php
@@ -24,96 +24,90 @@
 <body class='pbody'>
 
 <?php
-        ini_set('display_errors', 1);
-        ini_set('display_startup_errors', 1);
-        error_reporting(E_ALL);
+    ini_set('display_errors', 1);
+    ini_set('display_startup_errors', 1);
+    error_reporting(E_ALL);
+
+    $ini_array = parse_ini_file("/home/pi/pi-heating-hub/config/config.ini", true);
+    
+    $servername = $ini_array['db']['server'];
+    $username =$ini_array['db']['user'];
+    $password = $ini_array['db']['password'];
+    $dbname = $ini_array['db']['database'];
+
+    #print_r($_GET);
+    $NETDEV_ID = $_GET['id'];
+
+    #echo $DEVICE_ID;
+    #echo $_SERVER["REQUEST_METHOD"];
         
-        $servername = "localhost";
-        $username = "pi";
-        $password = "password";
-        $dbname = "pi_heating_db";
-        
-        #print_r($_GET);
-        $NETDEV_ID = $_GET['id'];
-        
-        #echo $DEVICE_ID;
-        #echo $_SERVER["REQUEST_METHOD"];
-        
-        if ($_SERVER["REQUEST_METHOD"] == "POST" ) {
-                #print_r("<pre><BR>------------------------<BR>");
-                #print_r($_POST);
-                #print_r("<BR>------------------------<BR></pre>");
-                
-                #if ( isset($_POST["done"]) ) {
-                #        #echo "#### done ####";
-                #        header('Location: /devices-list.php');
-                #        exit();
-                #}
-                
-                if ( isset($_POST["save"]) ) {
-                        #echo "#### save ####";
-                        
-                        $POST_NETDEV_NAME = str_replace( "'", " ", $_POST["name"]);
-                        $POST_NETDEV_MAC = str_replace( " ", "", $_POST["mac"]);
-                        
-                        // Create connection
-                        $conn = mysqli_connect($servername, $username, $password, $dbname);
-                        // Check connection
-                        if (!$conn) {
-                                die("<br><br>Connection failed: " . mysqli_connect_error());
-                        }
-                        # Update schedules with post data
-                        $sql = "UPDATE network SET name = '$POST_NETDEV_NAME', mac = '$POST_NETDEV_MAC' WHERE id='$NETDEV_ID';";
-                        #echo $sql;
-                        if (mysqli_query($conn, $sql)) {
-                                #echo "<br><br>Schedule updated successfully";
-                        } else {
-                                echo "<br><br>Error: " . $sql . "<br>" . mysqli_error($conn);
-                        }
-                        mysqli_close($conn);
-                }
-                
-        }
-        
-        // Create connection
-        $conn = mysqli_connect($servername, $username, $password, $dbname);
-        // Check connection
-        if (!$conn) {
+    if ($_SERVER["REQUEST_METHOD"] == "POST" ) {
+
+        if ( isset($_POST["save"]) ) {
+            #echo "#### save ####";
+
+            $POST_NETDEV_NAME = str_replace( "'", " ", $_POST["name"]);
+            $POST_NETDEV_MAC = str_replace( " ", "", $_POST["mac"]);
+
+            // Create connection
+            $conn = mysqli_connect($servername, $username, $password, $dbname);
+            // Check connection
+            if (!$conn) {
                 die("<br><br>Connection failed: " . mysqli_connect_error());
+            }
+            # Update schedules with post data
+            $sql = "UPDATE network SET name = '$POST_NETDEV_NAME', mac = '$POST_NETDEV_MAC' WHERE id='$NETDEV_ID';";
+            #echo $sql;
+            if (mysqli_query($conn, $sql)) {
+                    #echo "<br><br>Schedule updated successfully";
+            } else {
+                echo "<br><br>Error: " . $sql . "<br>" . mysqli_error($conn);
+            }
+            mysqli_close($conn);
         }
-        echo '<form method="post" action="'.htmlspecialchars($_SERVER["PHP_SELF"]).'?id='.$NETDEV_ID.'">';
-        $sql = "SELECT * FROM network WHERE id=".$NETDEV_ID;
-        #echo $sql;
-        $result = mysqli_query($conn, $sql);
-        #print_r( $result );
-        if (mysqli_num_rows($result) == 0) {
-                echo "0 results";
-        }
-        
-        $row = mysqli_fetch_assoc($result);
-        $NETDEV_NAME = htmlentities($row["name"]);
-        $NETDEV_MAC = $row["mac"];
-        
-        echo "<span class='ptitle'>EDIT Network Device '$NETDEV_NAME'</span><br><br>";
-        echo "<table class='ttab'>";
-        echo "<tr><td>";
-        echo "<span class='tspan'>Name</span><br>";
-        echo "<input type='text' name='name' value='".$NETDEV_NAME."' class='itextbox'><br><br>";
-        
-        echo "</td></tr><tr><td>";
-        
-        echo "<span class='tspan'>MAC Address</span><br>";
-        echo "<input type='text' name='mac' value='$NETDEV_MAC' class='itextbox'><br><br>";
- 
-        echo "</td></tr>";
-        
-        echo "</table>";
-        
-        echo "<input type='submit' name='save' value='Save' class='bgreen' />";
-        echo "&nbsp;&nbsp;";
-        echo "<input type='button' onclick='location.href=\"/netdevices-list.php\";' value='Done' class='bgrey' />";
-        echo '</form>';
-        mysqli_close($conn);
+
+    }
+
+    // Create connection
+    $conn = mysqli_connect($servername, $username, $password, $dbname);
+    // Check connection
+    if (!$conn) {
+        die("<br><br>Connection failed: " . mysqli_connect_error());
+    }
+    echo '<form method="post" action="'.htmlspecialchars($_SERVER["PHP_SELF"]).'?id='.$NETDEV_ID.'">';
+    $sql = "SELECT * FROM network WHERE id=".$NETDEV_ID;
+    #echo $sql;
+    $result = mysqli_query($conn, $sql);
+    #print_r( $result );
+    if (mysqli_num_rows($result) == 0) {
+        echo "0 results";
+    }
+
+    $row = mysqli_fetch_assoc($result);
+    $NETDEV_NAME = htmlentities($row["name"]);
+    $NETDEV_MAC = $row["mac"];
+
+    echo "<span class='ptitle'>EDIT Network Device '$NETDEV_NAME'</span><br><br>";
+    echo "<table class='ttab'>";
+    echo "<tr><td>";
+    echo "<span class='tspan'>Name</span><br>";
+    echo "<input type='text' name='name' value='".$NETDEV_NAME."' class='itextbox'><br><br>";
+
+    echo "</td></tr><tr><td>";
+
+    echo "<span class='tspan'>MAC Address</span><br>";
+    echo "<input type='text' name='mac' value='$NETDEV_MAC' class='itextbox'><br><br>";
+
+    echo "</td></tr>";
+
+    echo "</table>";
+
+    echo "<input type='submit' name='save' value='Save' class='bgreen' />";
+    echo "&nbsp;&nbsp;";
+    echo "<input type='button' onclick='location.href=\"/netdevices-list.php\";' value='Done' class='bgrey' />";
+    echo '</form>';
+    
+    mysqli_close($conn);
 ?>
 
 </body>
