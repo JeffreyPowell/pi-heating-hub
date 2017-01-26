ac4c4bc03a6dab55bab83a863096baaea0ca0309
Jeff <Jffrypwll@googlemail.com>
2017-01-26 14:06:38 +0000

    Update status.php

--- www/status.php
+++ www/status.php
@@ -217,7 +217,7 @@
     $sql = "SELECT * FROM sensors;";
     $result = mysqli_query($conn, $sql);
     if (mysqli_num_rows($result) == 0) {
-        echo "sensors 0 results"; 
+        #echo "sensors 0 results"; 
     }
 
     while($row = mysqli_fetch_assoc($result)) {
